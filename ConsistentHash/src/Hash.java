bcbe1ac6ad2d97c6b970ce000392bb889e1cbfc8
leishi <lei.shi.10151@gmail.com>
2016-12-21 21:50:18 -0800

    use sha for hash

--- ConsistentHash/src/Hash.java
+++ ConsistentHash/src/Hash.java
@@ -1,3 +1,6 @@
+import java.nio.ByteBuffer;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
@@ -8,9 +11,9 @@ public class Hash {
 
 	private ConcurrentMap<Long, String> _circle = new ConcurrentHashMap<Long, String>();
 	private List<Long> _sortedHashes;
-	private int replica = 20; 
+	private int replica = 10; 
 	
-	public void add(String Server) {
+	public void add(String Server) throws NoSuchAlgorithmException {
 		for(int i=0;i<replica;i++){
 			this._circle.putIfAbsent(this.computeHash(i + Server + i), Server);
 		}
@@ -19,12 +22,16 @@ public class Hash {
 	}
 	
 	public void remove(String Server) {
-		if(this._circle.remove(Server) != null) {
-			this.updateSortedHashes();
-		}
+		this._circle.forEach( (key, val) -> {
+			if(val == Server) {
+				this._circle.remove(key, val);
+			}
+		});
+		
+		this.updateSortedHashes();
 	}
 	
-	public String get(String Key) {
+	public String get(String Key) throws NoSuchAlgorithmException {
 		
 		if(this._circle.isEmpty()) {
 			return "";
@@ -49,43 +56,44 @@ public class Hash {
 		_sortedHashes = hashes;
 	}
 	
-	private Long computeHash(String val) {
-		long h = 1125899906842597L; // prime
-	    int len = val.length();
-
-	    for (int i = 0; i < len; i++) {
-	      h = 31*h + val.charAt(i);
-	    }
-	    
-	    return h;
+	private Long computeHash(String val) throws NoSuchAlgorithmException {		
+		MessageDigest messageDigest = null;
+		messageDigest = MessageDigest.getInstance("SHA-256");
+		messageDigest.update(val.getBytes());
+		ByteBuffer bb = ByteBuffer.wrap(messageDigest.digest());
+		return bb.getLong();
 	}
 	
 	public static void main(String[] args) {
-		Hash hash = new Hash();
-		hash.add("server1");
-		hash.add("server2");
-		hash.add("server3");
-		
-		for(int i=0;i<10;i++) {
-			String key = i + "_key" + i;
-			System.out.println(String.format("[%s] -> [%s]", key, hash.get(key)));
-		}
-		
-		hash.add("server4");
-		hash.add("server5");
-		
-		System.out.println();
-		for(int i=0;i<10;i++) {
-			String key = i + "_key" + i;
-			System.out.println(String.format("[%s] -> [%s]", key, hash.get(key)));
-		}
-		
-		hash.remove("server4");
-		
-		System.out.println();
-		for(int i=0;i<10;i++) {
-			String key = i + "_key" + i;
-			System.out.println(String.format("[%s] -> [%s]", key, hash.get(key)));
+		try{
+			Hash hash = new Hash();
+			hash.add("server1");
+			hash.add("server2");
+			hash.add("server3");
+			
+			for(int i=0;i<10;i++) {
+				String key = i + "_key" + i;
+				System.out.println(String.format("[%s] -> [%s]", key, hash.get(key)));
+			}
+			
+			hash.add("server4");
+			hash.add("server5");
+			
+			System.out.println();
+			for(int i=0;i<10;i++) {
+				String key = i + "_key" + i;
+				System.out.println(String.format("[%s] -> [%s]", key, hash.get(key)));
+			}
+			
+			hash.remove("server4");
+			
+			System.out.println();
+			for(int i=0;i<10;i++) {
+				String key = i + "_key" + i;
+				System.out.println(String.format("[%s] -> [%s]", key, hash.get(key)));
+			}
+		} catch(Exception ex) {
+			System.out.println(ex);
 		}
 	}
 
